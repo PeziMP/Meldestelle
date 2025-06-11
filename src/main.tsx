d5f2a2a7b63f0e933a6dc2285056ef7b5d9a6fca
PeziMP <Petra.Manharter@hotmail.com>
2025-06-11 22:37:31 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TeilnehmerAnmeldung from './components/TeilnehmerAnmeldung';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <TeilnehmerAnmeldung />
+  </React.StrictMode>
+);
+
